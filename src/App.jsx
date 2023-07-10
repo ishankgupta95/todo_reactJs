42c672ef8f7b47f9e0ccd93d43b76b268c534dff
Ishank Gupta <ishank1995@gmail.com>
2023-07-10 19:55:03 +0530

    added BE code for application and tried FetchAPI in FE

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [todos, setTodos] = useState([])
+  console.log(typeof todos)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setTodos(json))
+  }, [])
+  console.log(todos)
+
   return (
     <>
-     Todo
+     {todos.map((todo) => <h1 key={todo.id}>{todo.title}</h1>)}
     </>
   )
 }
